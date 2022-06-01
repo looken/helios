1779cbd5fb3246c410e600ede4f2ef9081cc0d31
looken <18518007500@163.com>
2022-06-01 13:25:36 +0800

    server test

--- src/helios_service.cc
+++ src/helios_service.cc
@@ -2,11 +2,35 @@
 
 namespace helios {
 
+int HeliosServiceImpl::listBuckets (Client& client) {
+    //获取用户的Bucket列表
+    ListBucketsRequest listBucketsRequest;
+    ListBucketsResponse listBucketsResponse;
+    int ret = client.list_buckets(listBucketsRequest, &listBucketsResponse);
+    std::vector<BucketSummary> bucketSummaryList = listBucketsResponse.buckets();
+    if (listBucketsResponse.is_fail()) {
+        printf("error-message:%s\n", listBucketsResponse.error().message().c_str());
+        return ret;
+    }
+    //遍历Bucket
+    for(const BucketSummary& bs : bucketSummaryList){
+        std::cout << "bucketName: " << bs.name <<  std::endl;
+    }
+    return ret;
+}
+
 void HeliosServiceImpl::emoji(::google::protobuf::RpcController* controller,
        const helios::EmojiReq* req,
        helios::EmojiResp* resp,
        ::google::protobuf::Closure* done)
 {
+    FILE *logfp = fopen("log/sdk.log", "w");
+    sdk_set_log_stream(logfp);
+    sdk_set_log_level(SDK_LOG_DEBUG);
+
+    baidu::bos::cppsdk::Client client(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
+    listBuckets(client);
+
     done->Run();
 }
 
--- src/helios_service.h
+++ src/helios_service.h
@@ -1,9 +1,12 @@
 #ifndef helios_service_h
 #define helios_service_h
 
-#include <helios.pb.h>
+#include <bcesdk/bos/client.h>
 
+#include <helios.pb.h>
 #include <sofa/pbrpc/pbrpc.h>
+
+using namespace bcesdk_ns;
 
 namespace helios {
 
@@ -21,6 +24,12 @@ public:
 	       helios::EmojiResp* resp,
 	       ::google::protobuf::Closure* done);
 
+private:
+    int listBuckets (Client& client);
+
+private:
+    const std::string ACCESS_KEY_ID = "136da69842234cbdb8b91ed749e3cdee";
+    std::string SECRET_ACCESS_KEY = "3dbb8f6af0624679bb7608dac7bc97d3";
 };
 
 }
